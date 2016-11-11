692f3f5b392d37633e91ff39416b16a5101fc1a2
Vasily Postnicov <shamaz.mazum@gmail.com>
2016-11-11 21:25:34 +0300

    Correct fishki.net fetcher

--- fishki.lisp
+++ fishki.lisp
@@ -12,7 +12,12 @@
          (some (lambda (obj)
                  (and (stringp obj)
                       (search "picsw" obj)))
-             (cdr car)))))
+               (cdr car)))))
+
+(defun fishki-image/2-p (list)
+  ;; Also try find <div class="picture-relative">
+  (let ((car (car list)))
+    (equalp '(:div :class "picture-relative") car)))
 
 (defun get-file-source/fishki (list)
   (let ((a/img-tag (car list)))
@@ -20,11 +25,26 @@
         (getf (cdr a/img-tag) :src)
         (getf (cdr a/img-tag) :href))))
 
+(defun get-file-source/fishki/2 (list)
+  (getf
+   (cdar
+    (get-parameterized-tag
+     (cdr
+      (get-parameterized-tag (cdr list) :a))
+     :img))
+   :src))
+
 (defmethod image-sources ((thread fishki-thread))
-  (let ((files (search-in-tree (thread-body thread) #'fishki-image-p)))
-    (mapcar (lambda (file)
-              (let ((source (puri:merge-uris
-                             (puri:parse-uri (get-file-source/fishki file))
-                             (puri:parse-uri "http://fishki.net/"))))
-                (cons source (after-last-slash (puri:uri-path source)))))
-            files)))
+  (let ((files1 (search-in-tree (thread-body thread) #'fishki-image-p))
+        (files2 (search-in-tree (thread-body thread) #'fishki-image/2-p)))
+    (union
+     (mapcar (lambda (file)
+               (let ((source (puri:merge-uris
+                              (puri:parse-uri (get-file-source/fishki file))
+                              (puri:parse-uri "http://fishki.net/"))))
+                 (cons source (after-last-slash (puri:uri-path source)))))
+             files1)
+     (mapcar (lambda (file)
+               (let ((source (get-file-source/fishki/2 file)))
+                 (cons (puri:parse-uri source) (after-last-slash source))))
+             files2))))
