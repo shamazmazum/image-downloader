70583b10f34fd99b7910e047303fdca9393552fe
Vasily Postnicov <shamaz.mazum@gmail.com>
2022-09-25 06:30:03 +0300

    Use ALEX:ASSOC-VALUE

--- src/4chan.lisp
+++ src/4chan.lisp
@@ -20,26 +20,26 @@
 (defmethod download-resource :after ((thread 4chan-thread))
   (let ((first-post (cadr (find :posts (resource-body thread) :key #'car))))
     (setf (imageboard-thread-name thread)
-          (cdr (assoc :semantic--url first-post)))))
+          (alex:assoc-value first-post :semantic--url))))
 
 (defmethod image-sources ((thread 4chan-thread))
   (let (result)
     (mapc
      (lambda (post)
-       (let ((tim (cdr (assoc :tim post)))
-             (ext (cdr (assoc :ext post))))
-         (if (and tim ext)
-             (push
-              (make-instance 'image-md5
-                             :uri (make-instance 'puri:uri
-                                                 :scheme :https
-                                                 :host "i.4cdn.org"
-                                                 :path (format nil "/~a/~d~a"
-                                                               (imageboard-board thread) tim ext))
-                             :name (make-pathname :name (format nil "~d" tim)
-                                                  :type (subseq ext 1))
-                             :md5 (with-input-from-string (in (cdr (assoc :md-5 post)))
-                                    (s-base64:decode-base64-bytes in)))
-              result))))
-     (cdr (assoc :posts (resource-body thread))))
+       (let ((tim (alex:assoc-value post :tim))
+             (ext (alex:assoc-value post :ext)))
+         (when (and tim ext)
+           (push
+            (make-instance 'image-md5
+                           :uri (make-instance 'puri:uri
+                                               :scheme :https
+                                               :host "i.4cdn.org"
+                                               :path (format nil "/~a/~d~a"
+                                                             (imageboard-board thread) tim ext))
+                           :name (make-pathname :name (format nil "~d" tim)
+                                                :type (subseq ext 1))
+                           :md5 (with-input-from-string (in (cdr (assoc :md-5 post)))
+                                  (s-base64:decode-base64-bytes in)))
+            result))))
+     (alex:assoc-value (resource-body thread) :posts))
     result))
